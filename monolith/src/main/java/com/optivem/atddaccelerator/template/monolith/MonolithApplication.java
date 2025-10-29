928ae2a68a99d49f9904bc8f41fc9ea06a38173e
Andrew <andrewwa@kainos.com>
2025-10-29 12:15:18 +0000

    replace tabs with spaces

--- monolith/src/main/java/com/optivem/atddaccelerator/template/monolith/MonolithApplication.java
+++ monolith/src/main/java/com/optivem/atddaccelerator/template/monolith/MonolithApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MonolithApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MonolithApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MonolithApplication.class, args);
+    }
 
 }
